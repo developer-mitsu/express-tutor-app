15d3fb0f86c97e3ea569fa2bdf1e94d34c9e285e
Mitsu <developer.mitsu@gmail.com>
2019-09-19 14:33:33 +0900

    [Q1] 解答

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/hello', function(req, res) {
+  res.send('Hello, World');
+})
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
