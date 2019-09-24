b71297c605668ece52494e41d1acf5e9ae9a56e3
Mitsu <developer.mitsu@gmail.com>
2019-09-24 15:16:58 +0900

    [8]データベースドライバーのインストール

---
+++ public/javascripts/main.js
@@ -0,0 +1,7 @@
+// main.js
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    document.getElementById('btn').addEventListener('click', ()=>{
+        console.log('btn Clicked!')
+    })
+})
